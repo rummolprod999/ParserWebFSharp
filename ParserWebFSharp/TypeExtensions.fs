c5604badc67abea73ffb33c594f4649ae3770b41
rummolprod999 <rummolprod999@gmail.com>
2019-03-19 13:36:32 +0300

    fix rostend parser

--- ParserWebFSharp/TypeExtensions.fs
+++ ParserWebFSharp/TypeExtensions.fs
@@ -7,71 +7,71 @@ open OpenQA.Selenium
 
 module TypeE =
     open System.Collections.Generic
-    
+
     type System.String with
-        
+
         member this.DateFromString(pat : string) =
-            try 
+            try
                 Some(DateTime.ParseExact(this, pat, CultureInfo.InvariantCulture))
             with ex -> None
-        
+
         member this.DateFromString(pat : string, exc : string) =
             match this.DateFromString(pat) with
             | None -> Error(exc)
             | Some d -> Success(d)
-        
+
         member this.Get1FromRegexp(regex : string) : string option =
             match this with
             | Tools.RegexMatch1 regex gr1 -> Some(gr1)
             | _ -> None
-        
+
         member this.Get1FromRegexpOrDefaul(regex : string) : string =
             match this with
             | Tools.RegexMatch1 regex gr1 -> gr1
             | _ -> ""
-        
+
         member this.Get1Optional(regex : string) =
             match this.Get1FromRegexp(regex) with
             | None -> Success("")
             | Some e -> Success(e.Trim())
-        
+
         member this.Get1(regex : string, exc : string) =
             match this.Get1FromRegexp(regex) with
             | None -> Error(exc)
             | Some e -> Success(e.Trim())
-        
+
         member this.Get2FromRegexp(regex : string) : (string * string) option =
             match this with
             | Tools.RegexMatch2 regex (gr1, gr2) -> Some(gr1, gr2)
             | _ -> None
-        
+
         member this.Get2FromRegexpOptional(regex : string, exc : string) =
             match this with
             | Tools.RegexMatch2 regex (gr1, gr2) -> Success(gr1, gr2)
             | _ -> Error(exc)
-        
+
         member this.GetPriceFromString(?template) : string =
             let templ = defaultArg template @"([\d, ]+)"
             match this.Get1FromRegexp templ with
             | Some x -> Regex.Replace(x.Replace(",", ".").Trim(), @"\s+", "")
             | None -> ""
-        
+
         member this.DateFromStringRus(pat : string) =
-            try 
+            try
                 Some(DateTime.ParseExact(this, pat, CultureInfo.CreateSpecificCulture("ru-RU")))
-            with ex -> 
+            with ex ->
                 printfn "%O" this
                 printfn "%O" pat
                 None
-        
+
         member this.RegexReplace() = Regex.Replace(this, @"\s+", " ")
         member this.RegexDeleteWhitespace() = Regex.Replace(this, @"\s+", "")
         member this.RegexCutWhitespace() = Regex.Replace(this, @"\s+", " ")
-        
+
         member this.GetNmck() =
             let tmp = this.Replace(",", ".").RegexDeleteWhitespace()
             tmp
-        
+
         member this.ReplaceDate() =
             if this.Contains("января") then this.Replace(" января ", ".01.")
             elif this.Contains("февраля") then this.Replace(" февраля ", ".02.")
@@ -86,7 +86,7 @@ module TypeE =
             elif this.Contains("ноября") then this.Replace(" ноября ", ".11.")
             elif this.Contains("декабря") then this.Replace(" декабря ", ".12.")
             else this
-        
+
         member this.ReplaceDateAriba() =
             if this.Contains("янв") then this.Replace(" янв ", ".01.")
             elif this.Contains("фев") then this.Replace(" фев ", ".02.")
@@ -101,7 +101,7 @@ module TypeE =
             elif this.Contains("ноя") then this.Replace(" ноя ", ".11.")
             elif this.Contains("дек") then this.Replace(" дек ", ".12.")
             else this
-            
+
         member this.ReplaceDateAsgor() =
             if this.Contains("Января") then this.Replace("Января", "01")
             elif this.Contains("Февраля") then this.Replace("Февраля", "02")
@@ -116,81 +116,86 @@ module TypeE =
             elif this.Contains("Ноября") then this.Replace("Ноября", "11")
             elif this.Contains("Декабря") then this.Replace("Декабря", "12")
             else this
-    
+
     type HtmlAgilityPack.HtmlNode with
-        
+
         member this.Gsn(s : string) =
             match this.SelectSingleNode(s) with
             | null -> ""
             | e -> e.InnerText.Trim()
-        
+
         member this.GsnAtr (s : string) (atr : string) =
             match this.SelectSingleNode(s) with
             | null -> ""
-            | e -> 
+            | e ->
                 match e.Attributes.[atr] with
                 | null -> ""
                 | at -> at.Value.Trim()
-        
+
         member this.getAttrWithoutException (atr : string) =
             match this.Attributes.[atr] with
             | null -> ""
             | at -> at.Value.Trim()
-    
+
     type HtmlAgilityPack.HtmlNodeNavigator with
-        
+
         member this.Gsn(s : string) =
             match this.SelectSingleNode(s) with
             | null -> ""
             | e -> e.Value.Trim()
-        
+
         member this.GsnAtr (s : string) (atr : string) =
             match this.SelectSingleNode(s) with
             | null -> ""
-            | e -> 
+            | e ->
                 match e.GetAttribute(atr, "") with
                 | null -> ""
                 | at -> at.Trim()
-    
+
     type ISearchContext with
-        
+
         member this.findElementWithoutException (xpath : string) =
-            try 
+            try
                 let res = this.FindElement(By.XPath(xpath))
                 match res with
                 | null -> ""
                 | r -> r.Text.Trim()
             with ex -> ""
-        
+
         member this.findElementsWithoutException (xpath : string) =
-            try 
+            try
                 let res = this.FindElements(By.XPath(xpath))
                 match res with
-                | null -> 
+                | null ->
                     new Collections.ObjectModel.ReadOnlyCollection<IWebElement>((new List<IWebElement>()) :> IList<IWebElement>)
                 | r -> r
-            with ex -> 
+            with ex ->
                 new Collections.ObjectModel.ReadOnlyCollection<IWebElement>((new List<IWebElement>()) :> IList<IWebElement>)
-        
+
         member this.findElementWithoutException (xpath : string, exc : string) =
-            try 
+            try
                 let res = this.FindElement(By.XPath(xpath))
                 match res with
                 | null -> Error(exc)
                 | r -> Success(r.Text.Trim())
             with ex -> Error(exc)
-        
+
         member this.findElementWithoutExceptionOptional (xpath : string, exc : string) =
-            try 
+            try
                 let res = this.FindElement(By.XPath(xpath))
                 match res with
                 | null -> Success("")
                 | r -> Success(r.Text.Trim())
             with ex -> Success("")
-    
+
     type IWebElement with
         member this.findAttributeWithoutException (attr : string, exc : string) =
-            try 
+            try
                 let attr = this.GetAttribute(attr)
                 Success(attr)
             with ex -> Error(exc)
+
+    type List<'T> with
+        member x.RemoveAllFromList(ls : IEnumerable<_>) =
+            for el in ls do
+                x.Remove(el) |> ignore
