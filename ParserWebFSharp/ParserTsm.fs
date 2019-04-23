b4c261e875c31f8acd5d8874fb1442d305aad2f1
rummolprod999 <rummolprod999@gmail.com>
2019-04-23 15:17:33 +0300

    fix issue

--- ParserWebFSharp/ParserTsm.fs
+++ ParserWebFSharp/ParserTsm.fs
@@ -11,7 +11,7 @@ open OpenQA.Selenium.Support.UI
 type ParserTsm(stn : Settings.T) =
     inherit Parser()
     let set = stn
-    let timeoutB = TimeSpan.FromSeconds(30.)
+    let timeoutB = TimeSpan.FromSeconds(60.)
     let url = "https://tender.tsm.ru/trades?page=purchases"
     let listTenders = new List<SamoletRec>()
     let options = ChromeOptions()
