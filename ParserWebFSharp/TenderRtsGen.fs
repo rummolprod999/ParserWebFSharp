d917ef39116787c7c12832b567b584131a453834
rummolprod999 <rummolprod999@gmail.com>
2019-06-18 14:06:51 +0300

    add new place

--- ParserWebFSharp/TenderRtsGen.fs
+++ ParserWebFSharp/TenderRtsGen.fs
@@ -32,7 +32,7 @@ type TenderRtsGen(stn: Settings.T, tn: RtsGenRec, typeFz: int, etpName: string, 
                                                return! Err ""
                         reader.Close()
                         let Page = Download.DownloadString tn.Href
-                        if Page = "" || Page = null then return! Err (sprintf "%s" tn.Href)
+                        if Page = "" || Page = null then return! Err(sprintf "%s" tn.Href)
                         let htmlDoc = new HtmlDocument()
                         htmlDoc.LoadHtml(Page)
                         let nav = (htmlDoc.CreateNavigator()) :?> HtmlNodeNavigator
@@ -57,7 +57,7 @@ type TenderRtsGen(stn: Settings.T, tn: RtsGenRec, typeFz: int, etpName: string, 
                                 reader.Close()
                                 let addOrganizer = sprintf "INSERT INTO %sorganizer SET full_name = @full_name, contact_person = @contact_person, post_address = @post_address, fact_address = @fact_address, contact_phone = @contact_phone, inn = @inn, contact_email = @contact_email" stn.Prefix
                                 let contactPerson = InlineHtmlNavigator nav "//label[contains(., 'Ответственное должностное лицо')]/following-sibling::span"
-                                let postAddress = InlineHtmlNavigator nav "//label[contains(., 'Почтовый адрес')]/following-sibling::span]"
+                                let postAddress = InlineHtmlNavigator nav "//label[contains(., 'Почтовый адрес')]/following-sibling::span"
                                 let factAddress = InlineHtmlNavigator nav "//label[contains(., 'Место нахождения')]/following-sibling::span"
                                 let phone = InlineHtmlNavigator nav "//label[contains(., 'Телефон')]/following-sibling::span"
                                 let inn = InlineHtmlNavigator nav "//label[contains(., 'ИНН организации')]/following-sibling::span"
@@ -73,7 +73,7 @@ type TenderRtsGen(stn: Settings.T, tn: RtsGenRec, typeFz: int, etpName: string, 
                                 cmd5.ExecuteNonQuery() |> ignore
                                 IdOrg := int cmd5.LastInsertedId
                                 ()
-                                
+
                         let idEtp = this.GetEtp con settings
                         let numVersion = 1
                         let idPlacingWay = ref 0
@@ -81,7 +81,44 @@ type TenderRtsGen(stn: Settings.T, tn: RtsGenRec, typeFz: int, etpName: string, 
                         | "" -> ()
                         | _ -> idPlacingWay := this.GetPlacingWay con tn.PwayName settings
                         let idRegion = this.GetReginId con tn.RegionName settings
-                        
+                        let scoringDateT = InlineHtmlNavigator nav "//label[contains(., 'Дата и время расcмотрения заявок')]/following-sibling::span"
+                        let scoringDateS = match scoringDateT.Get1FromRegexp """(\d{2}\.\d{2}\.\d{4}.+\d{2}:\d{2})""" with
+                                           | Some p -> p
+                                           | None -> ""
+                        let scoringDate = match scoringDateS.DateFromString("d.MM.yyyy HH:mm") with
+                                          | None -> DateTime.MinValue
+                                          | Some d -> d
+                        let idTender = ref 0
+                        let insertTender = String.Format ("INSERT INTO {0}tender SET id_xml = @id_xml, purchase_number = @purchase_number, doc_publish_date = @doc_publish_date, href = @href, purchase_object_info = @purchase_object_info, type_fz = @type_fz, id_organizer = @id_organizer, id_placing_way = @id_placing_way, id_etp = @id_etp, end_date = @end_date, scoring_date = @scoring_date, bidding_date = @bidding_date, cancel = @cancel, date_version = @date_version, num_version = @num_version, notice_version = @notice_version, xml = @xml, print_form = @print_form, id_region = @id_region", stn.Prefix)
+                        let cmd9 = new MySqlCommand(insertTender, con)
+                        cmd9.Prepare()
+                        cmd9.Parameters.AddWithValue("@id_xml", tn.PurNum) |> ignore
+                        cmd9.Parameters.AddWithValue("@purchase_number", tn.PurNum) |> ignore
+                        cmd9.Parameters.AddWithValue("@doc_publish_date", tn.DatePub) |> ignore
+                        cmd9.Parameters.AddWithValue("@href", tn.Href) |> ignore
+                        cmd9.Parameters.AddWithValue("@purchase_object_info", tn.PurName) |> ignore
+                        cmd9.Parameters.AddWithValue("@type_fz", typeFz) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_organizer", !IdOrg) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_placing_way", !idPlacingWay) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_etp", idEtp) |> ignore
+                        cmd9.Parameters.AddWithValue("@end_date", tn.DateEnd) |> ignore
+                        cmd9.Parameters.AddWithValue("@scoring_date", scoringDate) |> ignore
+                        cmd9.Parameters.AddWithValue("@bidding_date", DateTime.MinValue) |> ignore
+                        cmd9.Parameters.AddWithValue("@cancel", cancelStatus) |> ignore
+                        cmd9.Parameters.AddWithValue("@date_version", dateUpd) |> ignore
+                        cmd9.Parameters.AddWithValue("@num_version", numVersion) |> ignore
+                        cmd9.Parameters.AddWithValue("@notice_version", tn.status) |> ignore
+                        cmd9.Parameters.AddWithValue("@xml", tn.Href) |> ignore
+                        cmd9.Parameters.AddWithValue("@print_form", Printform) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_region", idRegion) |> ignore
+                        cmd9.ExecuteNonQuery() |> ignore
+                        idTender := int cmd9.LastInsertedId
+                        match updated with
+                        | true -> incr TenderRtsGen.tenderUpCount
+                        | false -> incr TenderRtsGen.tenderCount
+                        this.GetLots(con, !idTender, htmlDoc)
+                        this.AddVerNumber con tn.PurNum stn typeFz
+                        this.TenderKwords con (!idTender) stn
                         return ""
                         }
         match res with
@@ -90,8 +127,20 @@ type TenderRtsGen(stn: Settings.T, tn: RtsGenRec, typeFz: int, etpName: string, 
                 | Err r -> Logging.Log.logger r
         ()
 
-    
-    
+
+
+    member private this.GetLots(con: MySqlConnection, idTender: int, doc: HtmlDocument) =
+        let lots = doc.DocumentNode.SelectNodes("//div[@id = 'tradeLotsList']/div[@class = 'tradeLotInfo labelminwidth']")
+        if lots <> null then
+            for l in lots do
+                try
+                    this.Lot(con, idTender, l)
+                with ex -> Logging.Log.logger (ex)
+        ()
+
+    member private this.Lot(con: MySqlConnection, idTender: int, doc: HtmlNode) =
+
+        ()
     member private this.SetCancelStatus(con: MySqlConnection, dateUpd: DateTime) =
         let mutable cancelStatus = 0
         let mutable updated = false
