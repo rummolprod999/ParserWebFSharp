8c660a8fe2012be3c5a1946ee23ea33955582e88
rummolprod999 <rummolprod999@gmail.com>
2020-07-26 18:38:20 +0300

    fix issue

--- ParserWebFSharp/ParserRusal.fs
+++ ParserWebFSharp/ParserRusal.fs
@@ -14,7 +14,7 @@ type ParserRusal(stn: Settings.T) =
     inherit Parser()
     let set = stn
 
-    let urls = [|"https://rusal.ru/suppliers/selection/build/build_list/?mode=curr"; "https://rusal.ru/suppliers/selection/freight/freight_list/?mode=curr"; "https://rusal.ru/suppliers/selection/transport/transport_list/?mode=curr"; "https://rusal.ru/suppliers/selection/ports/ports_list/?mode=curr"; "https://rusal.ru/suppliers/selection/other/other_list/?mode=curr"; "https://rusal.ru/suppliers/selection/mtr/mtr_list/?mode=curr"|]
+    let urls = [|"https://rusal.ru/suppliers/selection/zhd-avia-avto-konteynernye-perevozki/"; "hhttps://rusal.ru/suppliers/selection/materialno-tekhnicheskie-resursy/"; "https://rusal.ru/suppliers/selection/perevalka-i-ekspedirovanie-gruzov-v-morskikh-portakh/"; "https://rusal.ru/suppliers/selection/prochie-uslugi/"; "https://rusal.ru/suppliers/selection/stroitelno-montazhnye-i-remontnye-raboty/"; "https://rusal.ru/suppliers/selection/frakht/"|]
 
     override __.Parsing() =
         for url in urls do
@@ -31,7 +31,7 @@ type ParserRusal(stn: Settings.T) =
         | s ->
             let parser = new HtmlParser()
             let documents = parser.Parse(s)
-            let tens = documents.QuerySelectorAll("table.rgMasterTable tr[id ^= 'bx_']").ToList()
+            let tens = documents.QuerySelectorAll("tr.js-pagination-item").ToList()
             for t in tens do
                     try
                         __.ParsingTender t url
@@ -48,7 +48,7 @@ type ParserRusal(stn: Settings.T) =
             let! purNum = t.GsnDocWithError "td:nth-of-type(2)" <| sprintf "purNum not found %s %s " url (t.TextContent)
             let! datePubT = t.GsnDocWithError "td:nth-of-type(1)" <| sprintf "datePubT not found %s %s " url (t.TextContent)
             let! datePub = datePubT.DateFromStringDoc ("dd.MM.yyyy", sprintf "datePub not found %s %s " href datePubT)
-            let! endDateT = t.GsnDocWithError "td:nth-of-type(6)" <| sprintf "endDateT not found %s %s " url (t.TextContent)
+            let! endDateT = t.GsnDocWithError "td:nth-of-type(3)" <| sprintf "endDateT not found %s %s " url (t.TextContent)
             let! dateEnd = endDateT.DateFromStringDoc ("dd.MM.yyyy", sprintf "dateEnd not found %s %s " href endDateT)
             let tend = {  RusalRec.Href = href
                           DateEnd = dateEnd
