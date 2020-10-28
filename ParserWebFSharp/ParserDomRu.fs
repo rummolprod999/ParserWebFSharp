3615739c95d7f23f47b42d4b65640b21b97e1563
rummolprod999 <rummolprod999@gmail.com>
2020-10-28 19:09:10 +0300

    update

---
+++ ParserWebFSharp/ParserDomRu.fs
@@ -0,0 +1,25 @@
+namespace ParserWeb
+
+open System
+open System.Collections.Generic
+open System.Globalization
+open TypeE
+open AngleSharp.Dom
+open AngleSharp.Parser.Html
+open System.Linq
+
+type ParserDomRu(stn: Settings.T) =
+    inherit Parser()
+    let set = stn
+
+    let urls = [|"http://www.oaobhm.ru/tender/"|]
+
+    override __.Parsing() =
+        for url in urls do
+            try
+                __.ParsingPage(url)
+            with ex -> Logging.Log.logger ex
+        ()
+
+
+    member private __.ParsingPage(url: string) = ()
