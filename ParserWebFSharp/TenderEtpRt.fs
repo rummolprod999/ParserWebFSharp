cff36820a32e9660ecdcbe17119cc97d3c72fb7f
rummolprod999 <rummolprod999@gmail.com>
2021-01-20 20:17:34 +0300

    add new parser

--- ParserWebFSharp/TenderEtpRt.fs
+++ ParserWebFSharp/TenderEtpRt.fs
@@ -19,14 +19,146 @@ type TenderEtpRt(stn: Settings.T, tn: EtpRtRec, typeFz: int, etpName: string, et
         use con = new MySqlConnection(stn.ConStr)
         let res =
                    builder {
-                       printfn "%A" tn
-                       return ""
+                        let Page = Download.DownloadString tn.Href
+                        if Page = "" || Page = null then return! Err(sprintf "%s" tn.Href)
+                        let htmlDoc = HtmlDocument()
+                        htmlDoc.LoadHtml(Page)
+                        let nav = (htmlDoc.CreateNavigator()) :?> HtmlNodeNavigator
+                        let dateUpd = DateTime.Now
+                        let! purName = nav.GsnDocWithError "//h1" <| sprintf "purName not found %s" tn.Href
+                        let! datePubT = nav.GsnDocWithError "//div[@class = 'lot_publish_date']/strong" <| sprintf "datePubT not found %s" tn.Href
+                        let! datePub = datePubT.DateFromStringDoc("dd.MM.yyyy", sprintf "datePub not found %s %s " tn.Href (datePubT))
+                        let! dateEndT1 = nav.GsnDocWithError "//div[@class = 'lot_deadline']/strong" <| sprintf "dateEndT1 not found %s" tn.Href
+                        let! dateEndT2 = nav.GsnDocWithError "//div[@class = 'lot_deadline']/span" <| sprintf "dateEndT2 not found %s" tn.Href 
+                        let dateEndT = sprintf "%s %s" dateEndT1 dateEndT2
+                        let! dateEnd = dateEndT.DateFromStringDoc("dd.MM.yyyy HH:mm", sprintf "dateEnd not found %s %s " tn.Href (dateEndT))
+                        let! dateScoringT = nav.GsnDocWithError "//div[@class = 'lot_rez_date']/strong" <| sprintf "dateScoringT not found %s" tn.Href
+                        let! dateScoring = dateScoringT.DateFromStringDoc("dd.MM.yyyy", sprintf "dateScoring not found %s %s " tn.Href (datePubT))
+                        let status = InlineHtmlNavigator nav "//div[. = 'Статус']/following-sibling::div"
+                        con.Open()
+                        let selectTend =
+                            sprintf 
+                                "SELECT id_tender FROM %stender WHERE purchase_number = @purchase_number AND  doc_publish_date = @doc_publish_date AND type_fz = @type_fz AND end_date = @end_date AND notice_version = @notice_version" 
+                                stn.Prefix
+                        let cmd : MySqlCommand = new MySqlCommand(selectTend, con)
+                        cmd.Prepare()
+                        cmd.Parameters.AddWithValue("@purchase_number", tn.PurNum) |> ignore
+                        cmd.Parameters.AddWithValue("@doc_publish_date", datePub) |> ignore
+                        cmd.Parameters.AddWithValue("@type_fz", typeFz) |> ignore
+                        cmd.Parameters.AddWithValue("@end_date", dateEnd) |> ignore
+                        cmd.Parameters.AddWithValue("@notice_version", status) |> ignore
+                        let reader : MySqlDataReader = cmd.ExecuteReader()
+                        if reader.HasRows then reader.Close()
+                                               return! Err ""
+                        reader.Close()
+                        let (cancelStatus, updated) = this.SetCancelStatus(con, dateUpd)
+                        let Printform = tn.Href
+                        let IdOrg = ref 0
+                        let orgName = etpName
+                        if orgName <> "" then
+                            let selectOrg = sprintf "SELECT id_organizer FROM %sorganizer WHERE full_name = @full_name" stn.Prefix
+                            let cmd3 = new MySqlCommand(selectOrg, con)
+                            cmd3.Prepare()
+                            cmd3.Parameters.AddWithValue("@full_name", orgName) |> ignore
+                            let reader = cmd3.ExecuteReader()
+                            match reader.HasRows with
+                            | true ->
+                                reader.Read() |> ignore
+                                IdOrg := reader.GetInt32("id_organizer")
+                                reader.Close()
+                            | false ->
+                                reader.Close()
+                                let addOrganizer = sprintf "INSERT INTO %sorganizer SET full_name = @full_name, contact_person = @contact_person, post_address = @post_address, fact_address = @fact_address, contact_phone = @contact_phone, inn = @inn, contact_email = @contact_email" stn.Prefix
+                                let contactPerson = ""
+                                let postAddress = ""
+                                let factAddress = ""
+                                let phone = ""
+                                let inn = ""
+                                let email = ""
+                                let cmd5 = new MySqlCommand(addOrganizer, con)
+                                cmd5.Parameters.AddWithValue("@full_name", orgName) |> ignore
+                                cmd5.Parameters.AddWithValue("@contact_person", contactPerson) |> ignore
+                                cmd5.Parameters.AddWithValue("@post_address", postAddress) |> ignore
+                                cmd5.Parameters.AddWithValue("@fact_address", factAddress) |> ignore
+                                cmd5.Parameters.AddWithValue("@contact_phone", phone) |> ignore
+                                cmd5.Parameters.AddWithValue("@inn", inn) |> ignore
+                                cmd5.Parameters.AddWithValue("@contact_email", email) |> ignore
+                                cmd5.ExecuteNonQuery() |> ignore
+                                IdOrg := int cmd5.LastInsertedId
+                                ()
+                        let idEtp = this.GetEtp con stn
+                        let pwName = InlineHtmlNavigator nav "//div[. = 'Вид процедуры']/following-sibling::div"
+                        let idPlacingWay = ref 0
+                        match pwName with
+                        | "" -> ()
+                        | _ -> idPlacingWay := this.GetPlacingWay con pwName stn
+                        let idTender = ref 0
+                        let numVersion = 1
+                        let insertTender = String.Format ("INSERT INTO {0}tender SET id_xml = @id_xml, purchase_number = @purchase_number, doc_publish_date = @doc_publish_date, href = @href, purchase_object_info = @purchase_object_info, type_fz = @type_fz, id_organizer = @id_organizer, id_placing_way = @id_placing_way, id_etp = @id_etp, end_date = @end_date, scoring_date = @scoring_date, bidding_date = @bidding_date, cancel = @cancel, date_version = @date_version, num_version = @num_version, notice_version = @notice_version, xml = @xml, print_form = @print_form, id_region = @id_region", stn.Prefix)
+                        let cmd9 = new MySqlCommand(insertTender, con)
+                        cmd9.Prepare()
+                        cmd9.Parameters.AddWithValue("@id_xml", tn.PurNum) |> ignore
+                        cmd9.Parameters.AddWithValue("@purchase_number", tn.PurNum) |> ignore
+                        cmd9.Parameters.AddWithValue("@doc_publish_date", datePub) |> ignore
+                        cmd9.Parameters.AddWithValue("@href", tn.Href) |> ignore
+                        cmd9.Parameters.AddWithValue("@purchase_object_info", purName) |> ignore
+                        cmd9.Parameters.AddWithValue("@type_fz", typeFz) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_organizer", !IdOrg) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_placing_way", !idPlacingWay) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_etp", idEtp) |> ignore
+                        cmd9.Parameters.AddWithValue("@end_date", dateEnd) |> ignore
+                        cmd9.Parameters.AddWithValue("@scoring_date", dateScoring) |> ignore
+                        cmd9.Parameters.AddWithValue("@bidding_date", DateTime.MinValue) |> ignore
+                        cmd9.Parameters.AddWithValue("@cancel", cancelStatus) |> ignore
+                        cmd9.Parameters.AddWithValue("@date_version", dateUpd) |> ignore
+                        cmd9.Parameters.AddWithValue("@num_version", numVersion) |> ignore
+                        cmd9.Parameters.AddWithValue("@notice_version", status) |> ignore
+                        cmd9.Parameters.AddWithValue("@xml", tn.Href) |> ignore
+                        cmd9.Parameters.AddWithValue("@print_form", Printform) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_region", 0) |> ignore
+                        cmd9.ExecuteNonQuery() |> ignore
+                        idTender := int cmd9.LastInsertedId
+                        match updated with
+                        | true -> incr TenderEtpRt.tenderUpCount
+                        | false -> incr TenderEtpRt.tenderCount
+                        let idCustomer = ref 0
+                        let cusName = InlineHtmlNavigator nav "//div[contains(@class, 'row filials')]/span"
+                        if cusName <> "" then
+                            let selectCustomer =
+                                sprintf "SELECT id_customer FROM %scustomer WHERE full_name = @full_name" stn.Prefix
+                            let cmd3 = new MySqlCommand(selectCustomer, con)
+                            cmd3.Prepare()
+                            cmd3.Parameters.AddWithValue("@full_name", cusName) |> ignore
+                            let reader = cmd3.ExecuteReader()
+                            match reader.HasRows with
+                            | true ->
+                                reader.Read() |> ignore
+                                idCustomer := reader.GetInt32("id_customer")
+                                reader.Close()
+                            | false ->
+                                reader.Close()
+                                let insertCustomer =
+                                    sprintf "INSERT INTO %scustomer SET reg_num = @reg_num, full_name = @full_name, inn = @inn"
+                                        stn.Prefix
+                                let RegNum = Guid.NewGuid().ToString()
+                                let cmd14 = new MySqlCommand(insertCustomer, con)
+                                cmd14.Prepare()
+                                cmd14.Parameters.AddWithValue("@reg_num", RegNum) |> ignore
+                                cmd14.Parameters.AddWithValue("@full_name", cusName) |> ignore
+                                cmd14.Parameters.AddWithValue("@inn", "") |> ignore
+                                cmd14.ExecuteNonQuery() |> ignore
+                                idCustomer := int cmd14.LastInsertedId
+                        this.GetLots(con, !idTender, !idCustomer, htmlDoc)
+                        return ""
                    }
         match res with
                         | Succ _ -> ()
                         | Err e when e = "" -> ()
                         | Err r -> Logging.Log.logger r
     
+    member private this.GetLots(con: MySqlConnection, idTender: int, idCustomer, doc: HtmlDocument) =
+        
+        ()
     member private this.SetCancelStatus(con: MySqlConnection, dateUpd: DateTime) =
         let mutable cancelStatus = 0
         let mutable updated = false
