afdcee40ad6f4351521e5eeb978f3feb50177474
rummolprod999 <rummolprod999@gmail.com>
2021-04-14 06:32:27 +0300

    add new place

--- ParserWebFSharp/TenderMagnitStroy.fs
+++ ParserWebFSharp/TenderMagnitStroy.fs
@@ -22,14 +22,181 @@ type TenderMagnitStroy(stn: Settings.T, tn: MagnitStroyRec, typeFz: int, etpName
         use con = new MySqlConnection(stn.ConStr)
         let res =
                    builder {
-                       printfn "%A" tn
-                       return ""
+                        con.Open()
+                        let selectTend =
+                            sprintf 
+                                "SELECT id_tender FROM %stender WHERE purchase_number = @purchase_number AND type_fz = @type_fz AND end_date = @end_date AND notice_version = @notice_version" 
+                                stn.Prefix
+                        let cmd : MySqlCommand = new MySqlCommand(selectTend, con)
+                        cmd.Prepare()
+                        cmd.Parameters.AddWithValue("@purchase_number", tn.PurNum) |> ignore
+                        cmd.Parameters.AddWithValue("@type_fz", typeFz) |> ignore
+                        cmd.Parameters.AddWithValue("@end_date", tn.DateEnd) |> ignore
+                        cmd.Parameters.AddWithValue("@notice_version", tn.Status) |> ignore
+                        let reader : MySqlDataReader = cmd.ExecuteReader()
+                        if reader.HasRows then reader.Close()
+                                               return! Error ""
+                        reader.Close()
+                        let dateUpd = DateTime.Now
+                        let (cancelStatus, updated) = this.SetCancelStatus(con, dateUpd)
+                        let wait = WebDriverWait(driver, TimeSpan.FromSeconds(60.))
+                        driver.Navigate().GoToUrl(tn.Href)
+                        Thread.Sleep(1000)
+                        driver.SwitchTo().DefaultContent() |> ignore
+                        wait.Until (fun dr -> dr.FindElement(By.XPath("//small[. = ' Информация о тендере']")).Displayed) |> ignore
+                        let body = driver.FindElement(By.XPath("//body"))
+                        let Printform = tn.Href
+                        let IdOrg = ref 0
+                        let orgName = etpName
+                        if orgName <> "" then
+                            let selectOrg = sprintf "SELECT id_organizer FROM %sorganizer WHERE full_name = @full_name" stn.Prefix
+                            let cmd3 = new MySqlCommand(selectOrg, con)
+                            cmd3.Prepare()
+                            cmd3.Parameters.AddWithValue("@full_name", orgName) |> ignore
+                            let reader = cmd3.ExecuteReader()
+                            match reader.HasRows with
+                            | true ->
+                                reader.Read() |> ignore
+                                IdOrg := reader.GetInt32("id_organizer")
+                                reader.Close()
+                            | false ->
+                                reader.Close()
+                                let addOrganizer = sprintf "INSERT INTO %sorganizer SET full_name = @full_name, contact_person = @contact_person, post_address = @post_address, fact_address = @fact_address, contact_phone = @contact_phone, inn = @inn, contact_email = @contact_email" stn.Prefix
+                                let contactPerson = tn.ContactPerson
+                                let postAddress = ""
+                                let factAddress = ""
+                                let phone = ""
+                                let inn = ""
+                                let email = ""
+                                let cmd5 = new MySqlCommand(addOrganizer, con)
+                                cmd5.Parameters.AddWithValue("@full_name", orgName) |> ignore
+                                cmd5.Parameters.AddWithValue("@contact_person", contactPerson) |> ignore
+                                cmd5.Parameters.AddWithValue("@post_address", postAddress) |> ignore
+                                cmd5.Parameters.AddWithValue("@fact_address", factAddress) |> ignore
+                                cmd5.Parameters.AddWithValue("@contact_phone", phone) |> ignore
+                                cmd5.Parameters.AddWithValue("@inn", inn) |> ignore
+                                cmd5.Parameters.AddWithValue("@contact_email", email) |> ignore
+                                cmd5.ExecuteNonQuery() |> ignore
+                                IdOrg := int cmd5.LastInsertedId
+                                ()
+                        let idEtp = this.GetEtp con settings
+                        let numVersion = 1
+                        let idPlacingWay = ref 0
+                        let idTender = ref 0
+                        let insertTender = String.Format ("INSERT INTO {0}tender SET id_xml = @id_xml, purchase_number = @purchase_number, doc_publish_date = @doc_publish_date, href = @href, purchase_object_info = @purchase_object_info, type_fz = @type_fz, id_organizer = @id_organizer, id_placing_way = @id_placing_way, id_etp = @id_etp, end_date = @end_date, scoring_date = @scoring_date, bidding_date = @bidding_date, cancel = @cancel, date_version = @date_version, num_version = @num_version, notice_version = @notice_version, xml = @xml, print_form = @print_form, id_region = @id_region", stn.Prefix)
+                        let cmd9 = new MySqlCommand(insertTender, con)
+                        cmd9.Prepare()
+                        cmd9.Parameters.AddWithValue("@id_xml", tn.PurNum) |> ignore
+                        cmd9.Parameters.AddWithValue("@purchase_number", tn.PurNum) |> ignore
+                        cmd9.Parameters.AddWithValue("@doc_publish_date", tn.DatePub) |> ignore
+                        cmd9.Parameters.AddWithValue("@href", tn.Href) |> ignore
+                        cmd9.Parameters.AddWithValue("@purchase_object_info", tn.PurName) |> ignore
+                        cmd9.Parameters.AddWithValue("@type_fz", typeFz) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_organizer", !IdOrg) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_placing_way", !idPlacingWay) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_etp", idEtp) |> ignore
+                        cmd9.Parameters.AddWithValue("@end_date", tn.DateEnd) |> ignore
+                        cmd9.Parameters.AddWithValue("@scoring_date", DateTime.MinValue) |> ignore
+                        cmd9.Parameters.AddWithValue("@bidding_date", DateTime.MinValue) |> ignore
+                        cmd9.Parameters.AddWithValue("@cancel", cancelStatus) |> ignore
+                        cmd9.Parameters.AddWithValue("@date_version", dateUpd) |> ignore
+                        cmd9.Parameters.AddWithValue("@num_version", numVersion) |> ignore
+                        cmd9.Parameters.AddWithValue("@notice_version", tn.Status) |> ignore
+                        cmd9.Parameters.AddWithValue("@xml", tn.Href) |> ignore
+                        cmd9.Parameters.AddWithValue("@print_form", Printform) |> ignore
+                        cmd9.Parameters.AddWithValue("@id_region", 0) |> ignore
+                        cmd9.ExecuteNonQuery() |> ignore
+                        idTender := int cmd9.LastInsertedId
+                        match updated with
+                        | true -> incr TenderMagnitStroy.tenderUpCount
+                        | false -> incr TenderMagnitStroy.tenderCount
+                        let idLot = ref 0
+                        let insertLot = sprintf "INSERT INTO %slot SET id_tender = @id_tender, lot_number = @lot_number, max_price = @max_price, currency = @currency, finance_source = @finance_source" stn.Prefix
+                        let cmd12 = new MySqlCommand(insertLot, con)
+                        cmd12.Parameters.AddWithValue("@id_tender", !idTender) |> ignore
+                        cmd12.Parameters.AddWithValue("@lot_number", 1) |> ignore
+                        cmd12.Parameters.AddWithValue("@max_price", tn.Nmck) |> ignore
+                        cmd12.Parameters.AddWithValue("@currency", "") |> ignore
+                        cmd12.Parameters.AddWithValue("@finance_source", "") |> ignore
+                        cmd12.ExecuteNonQuery() |> ignore
+                        idLot := int cmd12.LastInsertedId
+                        let idCustomer = ref 0
+                        let cusName = etpName
+                        if cusName <> "" then
+                            let selectCustomer =
+                                sprintf "SELECT id_customer FROM %scustomer WHERE full_name = @full_name" stn.Prefix
+                            let cmd3 = new MySqlCommand(selectCustomer, con)
+                            cmd3.Prepare()
+                            cmd3.Parameters.AddWithValue("@full_name", cusName) |> ignore
+                            let reader = cmd3.ExecuteReader()
+                            match reader.HasRows with
+                            | true ->
+                                reader.Read() |> ignore
+                                idCustomer := reader.GetInt32("id_customer")
+                                reader.Close()
+                            | false ->
+                                reader.Close()
+                                let insertCustomer =
+                                    sprintf "INSERT INTO %scustomer SET reg_num = @reg_num, full_name = @full_name, inn = @inn"
+                                        stn.Prefix
+                                let RegNum = Guid.NewGuid().ToString()
+                                let inn = ""
+                                let cmd14 = new MySqlCommand(insertCustomer, con)
+                                cmd14.Prepare()
+                                cmd14.Parameters.AddWithValue("@reg_num", RegNum) |> ignore
+                                cmd14.Parameters.AddWithValue("@full_name", cusName) |> ignore
+                                cmd14.Parameters.AddWithValue("@inn", inn) |> ignore
+                                cmd14.ExecuteNonQuery() |> ignore
+                                idCustomer := int cmd14.LastInsertedId
+                        let! delivTerm = body.findElementWithoutExceptionOptional("//h4[. = 'Описание тендера:']/following-sibling::div[1]", sprintf <|"delivTerm not found %s" <| tn.Href)
+                        if delivTerm <> "" then
+                            let insertCustomerRequirement =
+                                sprintf
+                                    "INSERT INTO %scustomer_requirement SET id_lot = @id_lot, id_customer = @id_customer, delivery_place = @delivery_place, delivery_term = @delivery_term"
+                                    stn.Prefix
+                            let cmd16 = new MySqlCommand(insertCustomerRequirement, con)
+                            cmd16.Prepare()
+                            cmd16.Parameters.AddWithValue("@id_lot", !idLot) |> ignore
+                            cmd16.Parameters.AddWithValue("@id_customer", !idCustomer) |> ignore
+                            cmd16.Parameters.AddWithValue("@delivery_place", "") |> ignore
+                            cmd16.Parameters.AddWithValue("@delivery_term", delivTerm.Trim()) |> ignore
+                            cmd16.ExecuteNonQuery() |> ignore
+                        let insertLotitem = sprintf "INSERT INTO %spurchase_object SET id_lot = @id_lot, id_customer = @id_customer, name = @name, okpd_name = @okpd_name, quantity_value = @quantity_value, customer_quantity_value = @customer_quantity_value, okei = @okei, sum = @sum" stn.Prefix
+                        let cmd19 = new MySqlCommand(insertLotitem, con)
+                        cmd19.Prepare()
+                        cmd19.Parameters.AddWithValue("@id_lot", !idLot) |> ignore
+                        cmd19.Parameters.AddWithValue("@id_customer", !idCustomer) |> ignore
+                        cmd19.Parameters.AddWithValue("@name", tn.PurName) |> ignore
+                        cmd19.Parameters.AddWithValue("@okpd_name", "") |> ignore
+                        cmd19.Parameters.AddWithValue("@quantity_value", "") |> ignore
+                        cmd19.Parameters.AddWithValue("@customer_quantity_value", "") |> ignore
+                        cmd19.Parameters.AddWithValue("@okei", "") |> ignore
+                        cmd19.Parameters.AddWithValue("@sum", tn.Nmck) |> ignore
+                        cmd19.ExecuteNonQuery() |> ignore
+                        let attachments = body.findElementsWithoutException("//a[starts-with(@href, '/projects-jb/latest-projects-jb/project.download')]")
+                        this.GetAttachments(con, !idTender, attachments)
+                        return ""
                    }
         match res with
                         | Success _ -> ()
                         | Error e when e = "" -> ()
                         | Error r -> Logging.Log.logger r
-
+    
+    member private this.GetAttachments(con: MySqlConnection, idTender: int, att: ReadOnlyCollection<_>) =
+        for doc in att do
+            let urlDoc = doc.GetAttribute("href").Trim()
+            let url = urlDoc
+            let name = doc.Text.Trim()
+            let addAttach =
+                    sprintf 
+                        "INSERT INTO %sattachment SET id_tender = @id_tender, file_name = @file_name, url = @url, description = @description" 
+                        stn.Prefix
+            let cmd5 = new MySqlCommand(addAttach, con)
+            cmd5.Parameters.AddWithValue("@id_tender", idTender) |> ignore
+            cmd5.Parameters.AddWithValue("@file_name", name) |> ignore
+            cmd5.Parameters.AddWithValue("@url", url) |> ignore
+            cmd5.Parameters.AddWithValue("@description", "") |> ignore
+            cmd5.ExecuteNonQuery() |> ignore
     member private this.SetCancelStatus(con: MySqlConnection, dateUpd: DateTime) =
         let mutable cancelStatus = 0
         let mutable updated = false
